f45de7b786839231c15499f72df1ec9589ba84ef
EvilPug <3608690@mail.ru>
2020-06-29 14:57:43 +0300

    Выполнена вторая лабораторная работа

---
+++ lab_02/safe_c_array_t.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+
+class safe_c_array_t {
+
+private:
+
+    int size;
+    int* arr;
+
+public:
+
+    explicit safe_c_array_t(int n) {
+        size = n;
+        arr = new int[size];
+    }
+
+    int& operator[](int k) {
+        return arr[k];
+    }
+
+    ~safe_c_array_t() {
+        delete[]arr;
+    }
+
+    safe_c_array_t& operator=(const safe_c_array_t& arr2) {
+        if (this != &arr2) {
+            delete[] arr;
+            size = arr2.size;
+            arr = new int[size];
+            for (int i = 0; i < size; i++) {
+                arr[i] = arr2.arr[i];
+            }
+        }
+        return *this;
+    }
+
+    safe_c_array_t(const safe_c_array_t& origin) {
+
+        size = origin.size;
+        arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = origin.arr[i];
+        }
+    }
+
+    void fill_random() {
+        
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = rand();
+        }
+
+    }
+
+    int length() {
+        return sizeof(arr);
+    }
+
+};
+
+int main() {
+
+
+    safe_c_array_t rand_nums = safe_c_array_t(4);
+
+    rand_nums.fill_random();
+
+    safe_c_array_t rand_nums_copy = safe_c_array_t(rand_nums);
+
+    for (int i = 0; i < rand_nums.length(); i++)
+    {
+        std::cout << rand_nums[i] << ' ' << rand_nums_copy[i] << std::endl;
+    }
+
+    return 0;
+}
---
+++ lab_02/safe_long_long_t.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+
+class safe_long_long_t {
+
+private:
+    long long value;
+
+public:
+    safe_long_long_t(long long x) {
+        value = x;
+    }
+
+    long long get() {
+        return value;
+    }
+
+    safe_long_long_t(safe_long_long_t const& temp) {
+        value = temp.value;
+    }
+
+
+    void operator=(const safe_long_long_t& param0) {
+        value = param0.value;
+    }
+
+    safe_long_long_t operator++() {
+        return safe_long_long_t(++value);
+    }
+
+    safe_long_long_t operator++(int) {
+        return safe_long_long_t(value++);
+    }
+
+    safe_long_long_t operator--() {
+        return safe_long_long_t(--value);
+    }
+
+    safe_long_long_t operator--(int) {
+        return safe_long_long_t(value--);
+    }
+
+    void operator+=(const safe_long_long_t& param0) {
+        value += param0.value;
+    }
+
+    void operator-=(const safe_long_long_t& param0) {
+        value -= param0.value;
+    }
+
+    void operator*=(safe_long_long_t& param0) {
+        value *= param0.value;
+    }
+
+    void operator/=(const safe_long_long_t& param0) {
+        value /= param0.value;
+    }
+
+    void operator%=(const safe_long_long_t& param0) {
+        value %= param0.value;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const safe_long_long_t& param0);
+
+    safe_long_long_t operator+(const safe_long_long_t& param0, const safe_long_long_t& param1) {
+        safe_long_long_t current = param0;
+        current += param1;
+        return current;
+    }
+
+    safe_long_long_t operator-(const safe_long_long_t& param0, const safe_long_long_t& param1) {
+        safe_long_long_t current = param0;
+        current -= param1;
+        return current;
+    }
+
+    safe_long_long_t operator*(const safe_long_long_t& param0, safe_long_long_t param1) {
+        safe_long_long_t current = param0;
+        current *= param1;
+        return current;
+    }
+
+    safe_long_long_t operator/(const safe_long_long_t& param0, const safe_long_long_t& param1) {
+        safe_long_long_t current = param0;
+        current /= param1;
+        return current;
+    }
+
+    safe_long_long_t operator%(safe_long_long_t& param0, safe_long_long_t& param1) {
+        safe_long_long_t current = param0;
+        current %= param1;
+        return current;
+    }
+
+    bool operator==(safe_long_long_t param0, safe_long_long_t param1) {
+        return param0.get() == param1.get();
+    }
+
+    bool operator!=(const safe_long_long_t& param0, const safe_long_long_t& param1) {
+        return !(param0 == param1);
+    }
+
+    bool operator<(safe_long_long_t param0, safe_long_long_t param1) {
+        return param0.get() < param1.get();
+    }
+
+    bool operator>(safe_long_long_t param0, safe_long_long_t param1) {
+        return param0.get() > param1.get();
+    }
+
+    bool operator<=(safe_long_long_t param0, safe_long_long_t param1) {
+        return param0.get() <= param1.get();
+    }
+
+    bool operator>=(safe_long_long_t param0, safe_long_long_t param1) {
+        return param0.get() >= param1.get();
+    }
+
+    std::ostream& operator<<(std::ostream& outs, const safe_long_long_t& param0) {
+        return outs << param0.value;
+    }
+
+
+    std::istream& operator>>(std::istream& ins, const safe_long_long_t& param0) {
+        ins >> param0;
+        return ins;
+    }
+};
+
+
+
+
+int main()
+{
+    safe_long_long_t a(10);
+    safe_long_long_t b(20);
+    safe_long_long_t c(10);
+
+    if (a < b) {
+        std::cout << a + b << std::endl;
+    }
+    
+    if (a == c) {
+        std::cout << a - b << std::endl;
+    }
+    
+    std::cout << a * b << std::endl;
+    std::cout << a / b << std::endl;
+
+    std::cout << --a << std::endl;
+    std::cout << a++ << std::endl;
+
+    return 0;
+}
